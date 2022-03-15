2798ab2431cfde2ecce181e804e6fbbed6cbc962
Lionel <Lionel.Delsupexhe@gmail.com>
2022-03-15 13:50:01 +0100

    code cleaning

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const navbar = document.getElementsByTagName('nav')[0];
 
 window.addEventListener('scroll', () => {
     if (scrollY >= window.innerHeight) {
-        navbar.classList.remove('green-to-transparent');
-        navbar.classList.add('transparent-to-green');
-    } else if (navbar.classList.contains('transparent-to-green')) {
-        navbar.classList.remove('transparent-to-green');
-        navbar.classList.add('green-to-transparent');
+        navbar.classList.remove('bg-green-to-transparent');
+        navbar.classList.add('bg-transparent-to-green');
+    } else if (navbar.classList.contains('bg-transparent-to-green')) {
+        navbar.classList.remove('bg-transparent-to-green');
+        navbar.classList.add('bg-green-to-transparent');
     }
 });
